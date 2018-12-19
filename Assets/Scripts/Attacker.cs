b14027e6d5571e15d97089dd2e7c6ad33aa9b865
TelGerot <edward.k.robe@gmail.com>
2018-12-19 13:37:46 +0200

    Made attackers attack

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
 float currentSpeed = 1f;
+    GameObject currentTarget;
+    Animator animator;
 
 	// Use this for initialization
 	void Start () {
-	}	
+        animator = GetComponent<Animator>();
+    }	
 
 	void Update () {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime );
@@ -17,5 +20,21 @@ float currentSpeed = 1f;
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;        
+    }
+
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("IsAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
