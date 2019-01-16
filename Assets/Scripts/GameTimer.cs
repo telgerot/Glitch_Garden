05de1ee3fd9001a63fb3b3e9fa1a4a3819a4f4bc
telgerot <edward.k.robe@gmail.com>
2019-01-16 09:19:05 +0200

    Added level timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in seconds")][SerializeField] float levelTime = 10f;
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Level timer expired");
+        }
+    }
+}
